4bbdd42e7015eadf1cc5f400dd2e74489c5a882e
sunnyleeyun <sunnylee945@hotmail.com>
2021-11-04 23:12:18 +0800

    Add self test exer 1

---
+++ Chapter2/SelfTestExer.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+// #1
+int q1() {
+    int count = 0, limit = 10;
+    // a. true
+    // b. true
+    // c. true
+    // d. true
+    // e. false
+    // f. true
+    // g. false
+    // h. error
+    // i. true
+    // j. error
+    // k. false
+    // l. 1
+    return 0;
+}
