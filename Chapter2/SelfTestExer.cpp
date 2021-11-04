ab309067cfea1287111e259a27a4ea6971bd3d3f
sunnyleeyun <sunnylee945@hotmail.com>
2021-11-04 23:17:48 +0800

    Add self test exer 2

--- Chapter2/SelfTestExer.cpp
+++ Chapter2/SelfTestExer.cpp
@@ -18,3 +18,13 @@ int q1() {
     // l. 1
     return 0;
 }
+
+// #2
+int q2() {
+    // 1. 2 < x < 3 looks odd but is still legal, meaning (2 < x) < 3 
+    //    while (2 < x) is a bool representing 0 or 1, no matter what x is,
+    //    (2 < x) could only be 0 or 1 which is always < 3
+    //    thus 2 < x < 3 will only be true or 1.
+    // 2. Should be (2 < x && x < 3)
+    return 0;
+}
