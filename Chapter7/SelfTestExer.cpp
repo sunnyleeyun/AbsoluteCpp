e04e660d62e06187f692af580109615986f3d2f3
sunnyleeyun <sunnylee945@hotmail.com>
2021-11-18 23:27:42 +0800

    Add sef test exer 1 and 2

---
+++ Chapter7/SelfTestExer.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// #1
+int q1() {
+    // 1, 2, 4, 5 is legal
+    return 0;
+}
+
+// #2
+int q2() {
+    // A default constructor is a constructor that takes no argument.
+    // Not every class has a default constructor.
+    return 0;
+}
