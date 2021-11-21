7eec9d237e7ee2d76f70c33eccc2e733638dc576
sunnyleeyun <sunnylee945@hotmail.com>
2021-11-21 20:12:43 +0800

    Add chapter 10 display 10.6

---
+++ Chapter10/Display10_6.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+typedef int* IntPtr;
+
+int main() 
+{
+    IntPtr p;
+    int a[10];
+    int index;
+
+    for (index = 0 ; index < 10; index++)
+        a[index] = index;
+    
+    p = a;
+    for (index = 0; index < 10; index++)
+        cout << p[index] << " ";
+    cout << endl;
+
+    for (index = 0; index < 10; index++)
+        p[index] = p[index] + 1;
+
+    for (index = 0; index < 10; index++)
+        cout << a[index] << " ";
+    cout << endl;
+
+    return 0;
+} 
+// Note that changes to the array p are also changes to the array a
